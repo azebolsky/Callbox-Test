d4980faffebb1557a1f6af942c97cb89143af74a
ADAM <zebolsky.adam@gmail.com>
2021-03-30 09:48:37 -0500

    add comment

--- callbox-nav/script.js
+++ callbox-nav/script.js
@@ -1,6 +1,8 @@
 const inventoryDropdown = document.getElementsByClassName("inventory-dropdown");
 const inventory = document.getElementsByClassName("inventory");
 
+// jquery to toggle class from inventory to inventory.clicked
+// opens and closes the inventory dropdown
 $(inventoryDropdown).on("click", function () {
   $(inventory).toggleClass("clicked");
 });
